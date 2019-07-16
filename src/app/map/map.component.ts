f297fe77310f7af7d7bc7467b46834f0ca046b5f
John Aucar <john.aucar@nextgencode.io>
2019-07-16 16:04:34 -0500

    update packages, add location description

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -7,13 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
   markers: Marker[] = [
-    { address: 'Tyler,TX', lat: 32.3491, lng: -95.306141 },
-    { address: 'Texas Tech University', lat: 33.57883, lng: -101.88802 },
-    { address: 'Cape Town, ZA', lat: -33.92711, lng: 18.42006 },
-    { address: 'Seville, ES', lat: 37.389091, lng: -5.984459 },
+    { address: 'Tyler,TX', lat: 32.3491, lng: -95.306141, description: 'Home Town' },
+    {
+      address: 'Texas Tech University',
+      lat: 33.57883,
+      lng: -101.88802,
+      description: 'Undergrad, Lubbock, TX'
+    },
+    {
+      address: 'Cape Town, ZA',
+      lat: -33.92711,
+      lng: 18.42006,
+      description: 'Where I learned to code'
+    },
+    { address: 'Seville, ES', lat: 37.389091, lng: -5.984459, description: 'Study Abroad' },
     { address: 'Paris, FR', lat: 48.856613, lng: 2.352222 },
-    { address: 'Omaha Beach, FR', lat: 49.35042, lng: -0.82668 },
-    { address: 'Santiago de Compostela, ES', lat: 42.88062, lng: -8.54661 }
+    {
+      address: 'Omaha Beach, FR',
+      lat: 49.35042,
+      lng: -0.82668,
+      description: 'Visited the D-day memorial'
+    },
+    {
+      address: 'Santiago de Compostela, ES',
+      lat: 42.88062,
+      lng: -8.54661,
+      description: 'Hiked the Camino'
+    }
   ];
   centerLat = 0;
   centerLng = 0;
