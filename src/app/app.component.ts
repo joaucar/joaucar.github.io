5adc676ea1c1d7363fb24039d41bb74e7e345afe
John Aucar <john.aucar@nextgencode.io>
2019-08-29 13:42:30 -0500

    first animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { faGraduationCap, faGlasses, faRocket } from '@fortawesome/free-solid-svg-icons';
 import { Info } from './info';
+import { trigger, style, transition, state, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('firstView', [
+      state('in', style({ opacity: 1 })),
+      transition(':enter', [style({ opacity: 0 }), animate(3000)])
+    ]),
+    trigger('firstViewText', [
+      state('in', style({ transform: 'translateY(0)', opacity: 1 })),
+      transition(':enter', [style({ transform: 'translateY(100%)', opacity: 0 }), animate(1000)])
+    ])
+    // trigger('headerInLeft', [
+    //   state('in', style({ transform: 'translateX(0)', opacity: 1 })),
+    //   transition(':enter', [style({ transform: 'translateX(-100%)', opacity: 0 }), animate(2000)]),
+    //   transition(':leave', [animate(2000, style({ transform: 'translateX(100%)', opacity: 0 }))])
+    // ])
+  ]
 })
 export class AppComponent implements OnInit {
   title = 'Website';
@@ -45,8 +61,7 @@ export class AppComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   makeActive(selection) {
     this.activeClass = selection;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -15,6 +16,7 @@ import { FooterComponent } from './footer/footer.component';
   declarations: [AppComponent, MapComponent, ThanksComponent, FooterComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
     FontAwesomeModule,
