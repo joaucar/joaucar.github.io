b90d4c6b00024e3f725e6df6e9d9f619f6a8edd7
John Aucar <john.aucar@nextgencode.io>
2019-10-24 10:18:01 -0500

    narrow page scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,9 +33,9 @@ export class AppComponent implements OnInit {
         'Here you can find my educational credintials as well as my work experience and other knowledge that I have collected in my past.',
       text:
         'In May of 2019 I earned a Bachelors degree in Mechanical Engineering with minors in Mathematics and Philosophy. ' +
-        'In September of 2019 I will begin graduate school for Data Science with a Healthcare concentration. My interest with ' +
+        'I am currently studying Data Science with a Healthcare concentration at DePaul University in Chicago. My interest with ' +
         'technology began to take off when I studied front-end web development at iXperience in Cape Town. I\'ve continued to ' +
-        'practice and learn and now have a certificate in Machine Learning from Stanford.'
+        'practice and learn and now I know several programming languages and have a certificate in Machine Learning from Stanford.'
     },
     {
       title: 'Hobbies',
@@ -44,7 +44,8 @@ export class AppComponent implements OnInit {
         'Here you can find more about what I do in my free time including sports, hobbies, passions and what I am doing right now.',
       text:
         'I first discovered competative ultimate freisbee my freshman year of college and I have been playing ever since. ' +
-        'I like to play the guitar, although I\'m not very good.'
+        'I enjoy running because it is peaceful and keeps me in shape. ' +
+        'I like to listen to music and play the guitar, although I\'m not very good.'
     },
     {
       title: 'Future',
@@ -53,8 +54,9 @@ export class AppComponent implements OnInit {
         'Here you can find where I hope to go with my life and what I hope to accomplish with my time \
          as well as my predictions on the direction the world is heading.',
       text:
-        'I am a supporter of space exploration and self-driving cars. I have been listening to the podcast Anatomy of Next and some of ' +
-        'the technologies they talk about have me excited for the future. If you get a chance give it a listen.'
+        'Through my studies in the technology industry I have become a supporter of space exploration and self-driving cars. ' +
+        'I have been NASA TV and I am excited to see the next several years of space exploration with the Artemis missions and some of ' +
+        'the scientific advancements they talk about have me excited for the future. If you get a chance, check it out.'
     }
   ];
   activeClass: Info;
